2278f0b43f7b045b5851cf12377926817b943db0
seven sins <admin@lonecpp.com>
2018-01-28 14:15:48 +0800

    add commit...

--- module-thread/src/main/java/com/lonecpp/thread/learning/two/QueueDemo3.java
+++ module-thread/src/main/java/com/lonecpp/thread/learning/two/QueueDemo3.java
@@ -16,8 +16,23 @@ public class QueueDemo3 {
 		
 		/**
 		 * 1. LinkedBlockingQueue 基于链表的阻塞队列
-		 * 		如指定初始化长度, 超出长度会抛异常
+		 * 		如指定初始化长度, 使用add()添加元素, 超出长度会抛异常
 		 * 		LinkedBlockingQueue<String> q = new LinkedBlockingQueue<>(5);
+		 * put(e) 阻塞
+		 * add(e) 超出长度抛异常
+		 * offer(e, time, unit) 指定时间内不能添加到队长超时返回false
+		 * 
+		 * poll()和peek() 都是取头元素节点, 区别在于前者会删除元素, 后者不会
+		 * 
+		 * 详细说明:
+		 		add        增加一个元索                     	如果队列已满，则抛出一个IIIegaISlabEepeplian异常
+				remove     移除并返回队列头部的元素    	如果队列为空，则抛出一个NoSuchElementException异常
+				element    返回队列头部的元素             	如果队列为空，则抛出一个NoSuchElementException异常
+				offer      添加一个元素并返回true    如果队列已满，则返回false
+				poll       移除并返问队列头部的元素    	如果队列为空，则返回null
+				peek       返回队列头部的元素             	如果队列为空，则返回null
+				put        添加一个元素                      	如果队列满，则阻塞
+				take       移除并返回队列头部的元素     如果队列为空，则阻塞
 		 */
 		LinkedBlockingQueue<String> q = new LinkedBlockingQueue<>();
 		q.add("a");
