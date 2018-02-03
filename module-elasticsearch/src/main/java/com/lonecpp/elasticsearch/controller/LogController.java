8f86b81212400556e26162ad23338c6ea71a6e28
seven sins <admin@lonecpp.com>
2018-02-03 16:45:48 +0800

    log4j log to kafka

---
+++ module-elasticsearch/src/main/java/com/lonecpp/elasticsearch/controller/LogController.java
@@ -0,0 +1,21 @@
+package com.lonecpp.elasticsearch.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author seven sins
+ * @date 2018年2月3日 下午3:19:26
+ */
+@RestController
+public class LogController {
+	
+	static final Logger LOG = Logger.getLogger(LogController.class);
+	
+	@GetMapping("/log/test")
+	public Object logTest() {
+		LOG.info("log4j kafka");
+		return 1;
+	}
+}
